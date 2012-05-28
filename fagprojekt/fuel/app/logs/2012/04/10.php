fea4d82dbc12c13d4f1bd62cda4c80b5d267db8d
Joachim Jensen <jv@intox.dk>
2012-05-28 11:48:31 +0200

    app cleanup

--- fagprojekt/fuel/app/logs/2012/04/10.php
+++
@@ -1,5 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-Warning - 2012-04-10 16:20:55 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
-Warning - 2012-04-10 16:21:07 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
-Warning - 2012-04-10 16:21:12 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
