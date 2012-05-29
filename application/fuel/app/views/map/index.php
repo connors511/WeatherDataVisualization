42a0ab90519a6ac975490fdb998f72156d9b845b
Matthias Larsen <matthias@Serenity>
2012-05-29 12:05:01 +0200

    Windmill JS

--- application/fuel/app/views/map/index.php
+++ application/fuel/app/views/map/index.php
@@ -80,6 +80,8 @@ body { font-size: 62.5%; }
 			<link rel="stylesheet" href="http://static.jquery.com/ui/css/demo-docs-theme/ui.theme.css" type="text/css" media="all" />
 			<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
 			<script src="http://code.jquery.com/ui/1.8.19/jquery-ui.min.js" type="text/javascript"></script>
+<script src="http://code.leafletjs.com/leaflet-0.3.1/leaflet.js"></script>
+			<?php echo Asset::js('map.content.js'); ?>
 <script>
 $(function() {
 $( "#dialog-form" ).dialog({
@@ -93,68 +95,11 @@ $( "#dialog-form" ).dialog({
 </head>
 <body>
 <div id="map"></div>
-<script src="http://code.leafletjs.com/leaflet-0.3.1/leaflet.js"></script>
 <script type="text/javascript">
 
-var map = new L.Map('map');
-
-var cloudmade = new L.TileLayer('http://{s}.tile.cloudmade.com/6d31e7d426dc40368e1b7bd1f07c8aa6/997/256/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery ? <a href="http://cloudmade.com">CloudMade</a>',
-    maxZoom: 18
-});
-
-var pos;
-
-
-pos = new L.LatLng(56.200, 11.200); 
-
-map.setView(pos, 7).addLayer(cloudmade);
-//map.locate({setView: true, maxZoom: 9});
-
-var MyIcon = L.Icon.extend({
-    iconUrl: '<?php echo Asset::get_file('windmill.png','img'); ?>',
-    shadowUrl: null,
-    iconSize: new L.Point(64, 64),
-    shadowSize: null,
-    iconAnchor: new L.Point(32, 32),
-    popupAnchor: new L.Point(-3, -76)
-});
-
-var icon = new MyIcon();
-
-var markerpos = new L.LatLng(56.2,11.2);
-var marker = new L.Marker(markerpos, {icon: icon}), marker2 = new L.Marker(new L.LatLng(56.2,9.2), {icon: icon});
-
-map.addLayer(marker).addLayer(marker2);
-
-marker.on('click', function(e) {
-    var page = "chart/?lat=" + this.getLatLng().lat.toFixed(3) + "&lng=" + this.getLatLng().lng.toFixed(3);
-    var pagetitle = "chart";
-    var $dialog = $( "#dialog-form" )
-                .html('<iframe style="border: 0px; " src="' + page + '" width="100%" height="100%"></iframe>')
-                .dialog({
-                    autoOpen: false,
-                    modal: true,
-                    height: screen.height*0.7,
-                    width: screen.width*0.8,
-                    title: pagetitle,
-                    close: function(ev, ui) { $("#map").fadeTo("slow", 1); }
-                });
-                $("#map").fadeTo("slow", 0.3);
-                $dialog.dialog('open');
-    //alert("test " + '(' + this.getLatLng().lat.toFixed(3) + ', ' + this.getLatLng().lng.toFixed(3) + ')');
-});
-
-map.on('click', onMapClick);
-
-function onMapClick(e) {
-    var latlngStr = '(' + e.latlng.lat.toFixed(3) + ', ' + e.latlng.lng.toFixed(3) + ')';
-	var popup = new L.Popup();
-    popup.setLatLng(e.latlng);
-    popup.setContent("You clicked the map at " + latlngStr);
-
-    map.openPopup(popup);
-}
+// 56.2, 11.2        56.2, 9.2
+WDV.Settings.Windfarm.positions = [[56.2, 11.2], [56.2, 9.2], [57.2, 9.7], [55.2, 8.2], [55.9, 7.2], [58.2, 6.7], [58.9, 5.8]];
+WDV.Init();
 
 </script>
 <div id="dialog-form" title="Chart">
--- application/public/assets/js/map.content.js
+++ application/public/assets/js/map.content.js
@@ -14,17 +14,18 @@ WDV = {
 		
 	Init: function() {
 		if (!this._initialized) {
-			_map = new L.Map('map');
-			_cloudmade = new L.TileLayer('http://{s}.tile.cloudmade.com/6d31e7d426dc40368e1b7bd1f07c8aa6/997/256/{z}/{x}/{y}.png', {
+			WDV._map = new L.Map('map');
+			WDV._cloudmade = new L.TileLayer('http://{s}.tile.cloudmade.com/6d31e7d426dc40368e1b7bd1f07c8aa6/997/256/{z}/{x}/{y}.png', {
 				attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery ? <a href="http://cloudmade.com">CloudMade</a>',
 				maxZoom: 18
 			});
-			// Our view position
-			_pos = new L.LatLng(56.200, 11.200); 
-			// Set the view
-			_map.setView(_pos, 7).addLayer(_cloudmade);
 			
-			_iconTemplate = L.Icon.extend({
+			// Our view position
+			WDV._pos = new L.LatLng(56.200, 11.200); 
+			// Set the view
+			WDV._map.setView(WDV._pos, 7).addLayer(WDV._cloudmade);
+			
+			WDV._iconTemplate = L.Icon.extend({
 				iconUrl: WDV.Settings.Icon.iconUrl,
 				shadowUrl: WDV.Settings.Icon.shadowUrl,
 				iconSize: WDV.Settings.Icon.iconSize,
@@ -33,29 +34,27 @@ WDV = {
 				popupAnchor: WDV.Settings.Icon.popupAnchor
 			});
 			
-			WDV.InitWindfarms();
-			_map.on('click', function() {
+			WDV._map.on('click', function(e) {
 				var latlngStr = '(' + e.latlng.lat.toFixed(3) + ', ' + e.latlng.lng.toFixed(3) + ')';
 				var popup = new L.Popup();
 				popup.setLatLng(e.latlng);
 				popup.setContent("You clicked the map at " + latlngStr);
 
-				map.openPopup(popup);
+				WDV._map.openPopup(popup);
 			});
+			WDV.InitWindfarms();
 		}
 	},
 	InitWindfarms: function() {
 		this._windfarms = [];
 		for (i = 0; i < WDV.Settings.Windfarm.positions.length; i++) {
 			// Create the marker
-			this._windfarms[i] = new WDV.Obj.Marker();
-			this._windfarms[i].position = new L.LatLng(WDV.Settings.Windfarm.positions[i][0], WDV.Settings.Windfarm.positions[i][1]);
-			this._windfarms[i].marker = new L.Marker(this._windfarms[i].position, {
-				icon: this._iconTemplate
+			this._windfarms[i] = new L.Marker(new L.LatLng(WDV.Settings.Windfarm.positions[i][0], WDV.Settings.Windfarm.positions[i][1]), {
+				icon: new this._iconTemplate()
 			});
 			
 			// Click event
-			marker.on('click', function(e) {
+			this._windfarms[i].on('click', function(e) {
 				var page = "chart/?lat=" + this.getLatLng().lat.toFixed(3) + "&lng=" + this.getLatLng().lng.toFixed(3);
 				var $dialog = $( "#dialog-form" )
 				.html('<iframe style="border: 0px; " src="' + page + '" width="100%" height="100%"></iframe>')
@@ -64,23 +63,22 @@ WDV = {
 					modal: WDV.Settings.Marker.modal,
 					height: WDV.Settings.Marker.height,
 					width: WDV.Settings.Marker.width,
-					title: WDV.Settings.Marker.title,
+					title: WDV.Settings.Marker.title.replace('LAT', this.getLatLng().lat.toFixed(3)).replace('LNG', this.getLatLng().lng.toFixed(3)),
 					close: WDV.Settings.Marker.close
 				});
 				$("#map").fadeTo("slow", 0.3);
 				$dialog.dialog('open');
-			//alert("test " + '(' + this.getLatLng().lat.toFixed(3) + ', ' + this.getLatLng().lng.toFixed(3) + ')');
 			});
 			
 			// Put it on the map
-			this._map.addLayer(this._windfarms[i].marker);
+			WDV._map.addLayer(this._windfarms[i]);
 		}
 	}
 };
 
 WDV.Settings = {
 	Icon: {
-		iconUrl: 'test.png',
+		iconUrl: 'assets/img/windmill.png',
 		shadowUrl: null,
 		iconSize: new L.Point(64, 64),
 		shadowSize: null,
@@ -92,7 +90,7 @@ WDV.Settings = {
 		modal: true,
 		height: screen.height * 0.7,
 		width: screen.width * 0.8,
-		title: 'Chart',
+		title: 'Chart (LAT, LNG)',
 		close: function(ev, ui) {
 			$("#map").fadeTo("slow", 1);
 		}
@@ -102,6 +100,9 @@ WDV.Settings = {
 	}
 };
 
+WDV.Obj = {
+	
+};
 
 /* Objects */
 WDV.Obj.Marker = {
