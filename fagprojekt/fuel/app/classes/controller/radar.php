8a4b17379027d5741f5def89834131dcc92e69a0
Matthias Larsen <0510ml@mail.dk>
2012-05-29 15:50:30 +0200

    Implementation of new color map

--- fagprojekt/fuel/app/classes/controller/radar.php
+++ fagprojekt/fuel/app/classes/controller/radar.php
@@ -21,7 +21,7 @@ class Controller_Radar extends Controller
 	public function action_index()
 	{
                 
-                $res = DB::select('*')->from('file_wrk')->where('id',Input::get('id',1))->execute()->as_array();
+                $res = DB::select('*')->from('file_wrk')->where('id',Input::get('id',3))->execute()->as_array();
                 $res = $res[0];
                 if (!file_exists(DOCROOT.'assets/radar/'.$res['id'].'.png'))
                 {
@@ -33,7 +33,91 @@ class Controller_Radar extends Controller
                     
                     // Fill the image with transparent color 
                     $color = imagecolorallocatealpha($img,0x00,0x00,0x00,127); 
-                    imagefill($img, 0, 0, $color); 
+                    imagefill($img, 0, 0, $color);
+		    
+		    $_colors = array(
+				array(
+					64,
+					array(0,0,0)
+				     ),
+				array(
+					10,
+					array(140, 140, 140)
+				),
+				array(
+					10,
+					array(0, 255, 255)
+				),
+				array(
+					10,
+					array(0, 149, 255)
+				),
+				array(
+					10,
+					array(0, 0, 255)
+				),
+				array(
+					10,
+					array(0, 255, 0)
+				),
+				array(
+					10,
+					array(0, 200, 0)
+				),
+				array(
+					10,
+					array(0, 160, 0)
+				),
+				array(
+					11,
+					array(0, 110, 0)
+				),
+				array(
+					10,
+					array(255, 255, 0)
+				),
+				array(
+					10,
+					array(255, 200, 0)
+				),
+				array(
+					10,
+					array(255, 126, 4)
+				),
+				array(
+					10,
+					array(255, 0, 0)
+				),
+				array(
+					10,
+					array(200, 0, 4)
+				),
+				array(
+					10,
+					array(151, 0, 4)
+				),
+				array(
+					10,
+					array(255, 0, 255)
+				),
+				array(
+					10,
+					array(175,0,175)
+				),
+				array(
+					30,
+					array(255, 255, 255)
+				)
+			    );
+				
+	$cmap = array();
+	$index = 0;
+	foreach($_colors as $c)
+	{
+		$_col = imagecolorallocatealpha($img, $c[1][0], $c[1][1], $c[1][2],0);
+		$cmap = array_merge($cmap, array_fill($index, $c[0], $_col));
+		$index += $c[0];
+	}
                     
                     for($i = 0; $i < $res['east_uppb']; $i++)
                     {
@@ -42,8 +126,8 @@ class Controller_Radar extends Controller
                                     $tmp = $colors[$i * 240 + $j];
                                     if ($tmp != 0 && $tmp != 255) {
                                             // 255 = out of range, 0 = no clouds
-                                            $col = imagecolorallocatealpha($img, (int)$tmp, (int)$tmp, (int)$tmp,0);
-                                            imagesetpixel($img, $i, $j, $col);
+                                            //$col = imagecolorallocatealpha($img, $cmap[$tmp][0], $cmap[$tmp][1], $cmap[$tmp][2],0);
+                                            imagesetpixel($img, $i, $j, $cmap[$tmp]);
                                     }
                             }
                     }
